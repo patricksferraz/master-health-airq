86a1456ac1e01d9cac4d0bc8921d35a2e0cdd60a
patricksferraz <patrick.ferraz@outlook.com>
2020-05-17 10:58:59 -0300

    refactor(code): moves PySUS|description and adds dotenv in process_addr

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### preprocessing-datasets.ipynb
 
-For execution, it is necessary to use the PySUS tool made available through this [link](https://github.com/fccoelho/PySUS) or in the code directory present in this repository.
+For execution, it is necessary to use the PySUS tool made available through this [link](https://github.com/fccoelho/PySUS) or in the `code/utils` directory present in this repository.
 
 ### data-analysis.ipynb
 
--- code/utils/process_addr.py
+++ code/utils/process_addr.py
@@ -1,38 +1,45 @@
 from description import cities_mrs
+from dotenv import load_dotenv
+from os import environ, path
+
 import multiprocessing as mp
+
 # import pycep_correios as pc
 import pandas as pd
-import datetime
 import requests
 import time
 import glob
-import env
 import os
-import re
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
 
 def get_address(cep) -> {}:
     api = (
         'https://webmaniabr.com/api/1/cep/{}/'
-        f'?app_key={env.app_key}'
-        f'&app_secret={env.app_secret}'
+        f'?app_key={environ.get("APP_KEY")}'
+        f'&app_secret={environ.get("APP_SECRET")}'
     )
     return requests.get(api.format(cep)).json()
+
 
 def process_addr(row: int, df: pd.DataFrame) -> pd.DataFrame:
     aux = df.loc[[row]]
     cep = str(aux.loc[row, 'CEP'])
-#     addr = pc.get_address_from_cep(cep)
-    addr = get_address(cep) # blocked by flood
+    #     addr = pc.get_address_from_cep(cep)
+    addr = get_address(cep)  # blocked by flood
 
     aux.loc[row, 'BAIRRO_RES'] = addr.get('bairro', '')
-    
+
     id_city = str(aux.loc[row, 'MUNIC_RES'])
     aux.loc[row, 'MUNIC_RES'] = cities_mrs.get(id_city, id_city)
-    
+
     id_city = str(aux.loc[row, 'MUNIC_MOV'])
     aux.loc[row, 'MUNIC_MOV'] = cities_mrs.get(id_city, id_city)
-    
+
     return aux
+
 
 # Directory for output
 if not os.path.exists('.process'):
@@ -43,16 +50,15 @@ for file in files:
     df = pd.read_csv(file, low_memory=False, compression='gzip')
     pool = mp.Pool(mp.cpu_count())
     results = pool.starmap_async(
-        process_addr,
-        [(row, df) for row in range(len(df))]
+        process_addr, [(row, df) for row in range(len(df))]
     ).get()
     pool.close()
     pool.join()
-    
+
     out = f".process/{file.split('/')[-1]}"
     aux = pd.concat(list(results))
     aux.to_csv(out, index=False, compression='gzip')
-    
+
     os.remove(file)
     print(f"[INFO] Done {file} (processed)")
     time.sleep(10)
